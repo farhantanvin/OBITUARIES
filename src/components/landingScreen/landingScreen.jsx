5f7faee8ebe4000ad05c7581b8e8777d58cf9994
akibhasan50 <tonuakibhasan@gmail.com>
2022-05-27 21:15:37 +0600

    bux fixed

--- src/components/landingScreen/landingScreen.jsx
+++ src/components/landingScreen/landingScreen.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import PricingScreen from '../pricingScreen/pricingScreen';
-import HeroSection from './heroSection';
-import Styles from './landingScreen.module.scss';
-import PlanRemembranceSection from './planRemembranceSection';
-import RecentObituariesSection from './recentObituariesSection';
-import SearchObituariesSection from './searchObituariesSection';
+import React from "react";
+import ObituariesVideoScreen from "../obituariesVideoScreen/ObituariesVideoScreen";
+import PricingScreen from "../pricingScreen/pricingScreen";
+import HeroSection from "./heroSection";
+import Styles from "./landingScreen.module.scss";
+import RecentObituariesSection from "./recentObituariesSection";
+import SearchObituariesSection from "./searchObituariesSection";
 
 class LandingScreen extends React.Component {
-	render() {
-		return (
-			<div className={Styles.mainContainer}>
-				<HeroSection />
-				<SearchObituariesSection history={this.props.history} />
-				<RecentObituariesSection history={this.props.history} />
-				<PlanRemembranceSection history={this.props.history} />
-				<PricingScreen />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className={Styles.mainContainer}>
+        <HeroSection />
+        <div>
+          <ObituariesVideoScreen></ObituariesVideoScreen>
+        </div>
+
+        <SearchObituariesSection history={this.props.history} />
+        <RecentObituariesSection history={this.props.history} />
+        {/* <PlanRemembranceSection history={this.props.history} /> */}
+        <PricingScreen />
+      </div>
+    );
+  }
 }
 
 export default LandingScreen;
